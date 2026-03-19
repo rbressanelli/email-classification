814122e2acbc1c368e96b4cfee11e3d4f29879cf
Roberto Bressanelli <rbressanelli@gmail.com>
2026-03-19 13:07:57 -0300

    Refactor: deleted commented code

--- app/main.py
+++ app/main.py
@@ -1,31 +1,3 @@
-# from fastapi import FastAPI, Request
-# from fastapi.middleware.cors import CORSMiddleware
-# from fastapi.staticfiles import StaticFiles
-# from fastapi.templating import Jinja2Templates
-
-# from app.api.routes import router
-# from app.core.config import settings
-
-# app = FastAPI(title=settings.app_name)
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# app.include_router(router)
-
-# app.mount("/static", StaticFiles(directory="app/static"), name="static")
-# templates = Jinja2Templates(directory="app/templates")
-
-
-# @app.get("/app")
-# def application_shell(request: Request):
-#     return templates.TemplateResponse("index.html", {"request": request})
-
 from pathlib import Path
 
 from fastapi import FastAPI, Request
